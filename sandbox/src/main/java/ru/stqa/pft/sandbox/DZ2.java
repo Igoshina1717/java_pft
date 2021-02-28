b6d55aa0bdf4af94d2b3f778921e58074847ac1d
aiigoshi <igoshina1717@gmail.com>
2021-02-28 15:05:21 +0300

    ДЗ2.Реализован метод в классе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/DZ2.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/DZ2.java
@@ -2,14 +2,10 @@ package ru.stqa.pft.sandbox;
 
 public class DZ2 {
   public static void main(String[] args) {
-    Point p1 = new Point(2,78);
+    Point p1 = new Point(10,78);
     Point p2 = new Point(55,105);
     System.out.println("Расстояние между P1 c координатами " + p1.x + ", " + p1.y +
-            " и P2 с координатами "+ p2.x + ", " + p2.y +" = " + distance(p1,p2));
-
-  }
-  public static double distance(Point p1, Point p2){
-     return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y- p1.y)*(p2.y- p1.y));
+            " и P2 с координатами "+ p2.x + ", " + p2.y +" = " + p1.distance(p2));
 
   }
 }
