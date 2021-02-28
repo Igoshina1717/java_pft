0df4c82e29cdbcf2bc2132ecfb635577e2bbb664
aiigoshi <igoshina1717@gmail.com>
2021-02-28 14:40:49 +0300

    ДЗ2.Запускаемый класс, содержащий функцию

--- sandbox/src/main/java/ru/stqa/pft/sandbox/DZ2.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/DZ2.java
@@ -2,8 +2,8 @@ package ru.stqa.pft.sandbox;
 
 public class DZ2 {
   public static void main(String[] args) {
-    Point p1 = new Point(1,10);
-    Point p2 = new Point(5,100);
+    Point p1 = new Point(2,78);
+    Point p2 = new Point(55,105);
     System.out.println("Расстояние между P1 c координатами " + p1.x + ", " + p1.y +
             " и P2 с координатами "+ p2.x + ", " + p2.y +" = " + distance(p1,p2));
 
