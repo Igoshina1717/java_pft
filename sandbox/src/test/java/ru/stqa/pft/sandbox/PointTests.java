57764ba71662df1871da56812885e112b87c8327
aiigoshi <igoshina1717@gmail.com>
2021-03-01 19:11:13 +0300

    Тесты_Проверка расстояний между точками

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -12,5 +12,20 @@ public class PointTests {
     Assert.assertEquals(p2.distance(p1),13.0);
 
   }
+  @Test
+  public void testPoint1(){
+    Point p1 = new Point(10,12);
+    Point p2 = new Point(300, 240);
+    Assert.assertEquals(p2.distance(p1),Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y- p1.y)*(p2.y- p1.y)));
+
+  }
+  @Test
+  public void testPoint2(){
+    Point p1 = new Point(-5,-12);
+    Point p2 = new Point(-10, -24);
+    Assert.assertEquals(p2.distance(p1),13.0);
+
+  }
+
 
 }
