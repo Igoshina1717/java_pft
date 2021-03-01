3089b56cc5fbf58602dbfa1cfc66d0a3dc431c5b
aiigoshi <igoshina1717@gmail.com>
2021-03-01 19:19:28 +0300

    черновики

--- sandbox/src/main/java/ru/stqa/pft/sandbox/DZ2.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/DZ2.java
@@ -2,7 +2,7 @@ package ru.stqa.pft.sandbox;
 
 public class DZ2 {
   public static void main(String[] args) {
-    Point p1 = new Point(5,12);
+    Point p1 = new Point(-10,-120);
     Point p2 = new Point(10,24);
     System.out.println("Расстояние между P1 c координатами " + p1.x + ", " + p1.y +
             " и P2 с координатами "+ p2.x + ", " + p2.y +" = " + p1.distance(p2));
