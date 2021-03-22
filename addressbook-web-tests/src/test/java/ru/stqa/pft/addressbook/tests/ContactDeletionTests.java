a4492d0e95c61f35bd1a0845ee0be68024628b65
aiigoshi <igoshina1717@gmail.com>
2021-03-22 20:05:57 +0300

    Сравнение коллекций(Удаление контактов))

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -7,22 +7,25 @@ import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.List;
 
-public class ContactDeletionTests extends TestBase{
+public class ContactDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() {
     app.getNavigationHelper().gotoHomePage();
-    if (! app.getContactHelper().isThereAContact()){
+    if (!app.getContactHelper().isThereAContact()) {
       app.getContactHelper().createContact(new ContactData("Василий", "Репин", "Москва", "79112324433", "test@mail.ru"));
     }
     List<ContactData> before = app.getContactHelper().getContactList();
-    app.getContactHelper().selectContact(before.size()-1);
+    app.getContactHelper().selectContact(before.size() - 1);
     app.getContactHelper().delete();
     app.getContactHelper().accept();
     app.getNavigationHelper().gotoHomePage();
     List<ContactData> after = app.getContactHelper().getContactList();
-    Assert.assertEquals(after.size(), before.size()-1);
-      }
+    Assert.assertEquals(after.size(), before.size() - 1);
 
+    before.remove(before.size()-1);
+      Assert.assertEquals(before, after);
+
+  }
 
 }
