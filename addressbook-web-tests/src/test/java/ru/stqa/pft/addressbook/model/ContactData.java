87f14025ad04909e39eac4f663c6a1691f74f9d5
aiigoshi <igoshina1717@gmail.com>
2021-03-30 18:14:40 +0300

    5.9 Способы построения сложных локаторов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -9,6 +9,8 @@ public class ContactData {
   private String address;
   private String mobile;
   private String email;
+  private String home;
+  private String work;
 
 
   public int getId() {
@@ -78,6 +80,15 @@ public class ContactData {
     this.email = email;
     return this;
   }
+  public ContactData withHomePhone(String home) {
+    this.home =home;
+    return this;
+  }
+
+  public ContactData withWorkPhone(String work) {
+    this.work = work;
+    return this;
+  }
   @Override
   public String toString() {
     return "ContactData{" +
@@ -86,5 +97,6 @@ public class ContactData {
             ", lastName='" + lastName + '\'' +
             '}';
   }
+
 
 }
