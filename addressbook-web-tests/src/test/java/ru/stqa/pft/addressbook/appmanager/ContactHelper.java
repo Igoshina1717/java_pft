efe39f3d9a0fee41c7e6d692c2b9fff6b3ef9f3e
aiigoshi <igoshina1717@gmail.com>
2021-04-02 16:51:21 +0300

    ДЗ11

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -28,7 +28,12 @@ public class ContactHelper extends HelperBase {
     type(By.name("lastname"), contactData.getLastName());
     type(By.name("address"), contactData.getAddress());
     type(By.name("mobile"), contactData.getMobile());
+    type(By.name("home"), contactData.getHomePhone());
     type(By.name("email"), contactData.getEmail());
+    type(By.name("work"), contactData.getWorkPhone());
+    type(By.name("email"), contactData.getEmail());
+    type(By.name("email2"), contactData.getEmail2());
+    type(By.name("email3"), contactData.getEmail3());
 
   }
 
@@ -103,13 +108,34 @@ public class ContactHelper extends HelperBase {
     contactCache = new Contacts();
     List<WebElement> elements = wd.findElements(By.name("entry"));
     for (WebElement element : elements){
+      List<WebElement> cells = element.findElements(By.tagName("td"));
       String name = element.findElements(By.tagName("td")).get(2).getText();
       String lastName = element.findElements(By.tagName("td")).get(1).getText();
+      String address = element.findElements(By.tagName("td")).get(3).getText();
+      String allPhones = cells.get(5).getText();
+      String allEmail = cells.get(4).getText();
       int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-      contactCache.add(new ContactData().withId(id).withName(name).withLastName(lastName));
+      contactCache.add(new ContactData().withId(id).withName(name).withLastName(lastName).withAddress(address)
+              .withAllPhones(allPhones).withAllEmail(allEmail));
     }
     return contactCache;
   }
 
 
+  public ContactData contactInfoFromEditForm(int id, ContactData contact) {
+    wd.findElement(By.xpath(String.format("//tr[.//input[@value='%s']]/td[8]/a",id))).click();
+    String firstname = wd.findElement(By.name("firstname")).getAttribute("value");
+    String lastname = wd.findElement(By.name("lastname")).getAttribute("value");
+    String home = wd.findElement(By.name("home")).getAttribute("value");
+    String mobile = wd.findElement(By.name("mobile")).getAttribute("value");
+    String work = wd.findElement(By.name("work")).getAttribute("value");
+    String address = wd.findElement(By.name("address")).getAttribute("value");
+    String email = wd.findElement(By.name("email")).getAttribute("value");
+    String email2 = wd.findElement(By.name("email2")).getAttribute("value");
+    String email3 = wd.findElement(By.name("email3")).getAttribute("value");
+    wd.navigate().back();
+    return new ContactData().withId(contact.getId()).withName(firstname).withLastName(lastname).withAddress(address)
+            .withHomePhone(home).withMobile(mobile).withWorkPhone(work)
+            .withEmail(email).withEmail2(email2).withEmail3(email3);
+  }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -9,6 +9,12 @@ public class ContactData {
   private String address;
   private String mobile;
   private String email;
+  private String email2;
+  private String email3;
+  private String home;
+  private String work;
+  private String allPhones;
+  private String allEmail;
 
 
   public int getId() {
@@ -31,11 +37,28 @@ public class ContactData {
   public String getMobile() {
     return mobile;
   }
+  public String getWorkPhone() {
+    return work;
+  }
 
   public String getEmail() {
     return email;
   }
-
+  public String getEmail2() {
+    return email2;
+  }
+  public String getEmail3() {
+    return email3;
+  }
+  public String getHomePhone() {
+    return home;
+  }
+  public String getAllPhones() {
+    return allPhones;
+  }
+  public String getAllEmail() {
+    return allEmail;
+  }
   public ContactData withId(int id) {
     this.id = id;
     return this;
@@ -50,7 +73,49 @@ public class ContactData {
     this.lastName = lastName;
     return this;
   }
+  public ContactData withHomePhone(String home) {
+    this.home = home;
+    return this;
+  }
+  public ContactData withMobile(String mobile) {
+    this.mobile = mobile;
+    return this;
+  }
+  public ContactData withWorkPhone(String work) {
+    this.work = work;
+    return this;
+  }
 
+  public ContactData withAddress(String address) {
+    this.address = address;
+    return this;
+  }
+
+
+  public ContactData withEmail(String email) {
+    this.email = email;
+    return this;
+  }
+
+  public ContactData withEmail2(String email2) {
+    this.email2 = email2;
+    return this;
+  }
+
+  public ContactData withEmail3(String email3) {
+    this.email3 = email3;
+    return this;
+  }
+
+
+  public ContactData withAllPhones(String allPhones) {
+    this.allPhones = allPhones;
+    return this;
+  }
+  public ContactData withAllEmail(String allEmail) {
+    this.allEmail = allEmail;
+    return this;
+  }
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
@@ -64,20 +129,6 @@ public class ContactData {
     return Objects.hash(id, name, lastName);
   }
 
-  public ContactData withAddress(String address) {
-    this.address = address;
-    return this;
-  }
-
-  public ContactData withMobile(String mobile) {
-    this.mobile = mobile;
-    return this;
-  }
-
-  public ContactData withEmail(String email) {
-    this.email = email;
-    return this;
-  }
   @Override
   public String toString() {
     return "ContactData{" +
