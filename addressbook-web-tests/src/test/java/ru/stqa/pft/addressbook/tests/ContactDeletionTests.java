828f7c104487db634b24031681e96be7b7c1adf3
aiigoshi <igoshina1717@gmail.com>
2021-04-26 21:53:50 +0300

    Revert "8.2 Добавлен новый модуль для тестов для Mantis"
    This reverts commit 1e292592cbea68e37d0a8c20e06ab8f35bf7330b.

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -0,0 +1,35 @@
+package ru.stqa.pft.addressbook.tests;
+
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class ContactDeletionTests extends TestBase {
+
+  @BeforeMethod
+  public void ensurePreconditions(){
+    app.goTo().HomePage();
+    if ( app.db().contacts().size() == 0){
+      app.contact().create(new ContactData().withName("Иван").withLastName("Иванов").withAddress("Москва").withMobile("79995553535").withEmail("test@mail.ru"));
+    }
+  }
+
+
+  @Test
+  public void testGroupDeletion() {
+    Contacts before = app.db().contacts();
+    ContactData deletedContact = before.iterator().next();
+    app.contact().delete(deletedContact);
+    assertThat(app.contact().count(), equalTo(before.size()-1));
+    Contacts after = app.db().contacts();
+    assertThat(after, equalTo(before.without(deletedContact)));
+    verifyContactListInUI();
+
+  }
+
+}
